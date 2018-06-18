a146b4c13eed19560cc6d08c4c4b335f08f49dbf
jtrtj <jt.r@live.com>
2018-06-18 11:35:10 -0600

    add database instance variable to task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,9 @@ require 'sqlite3'
 
 class Task
     def initialize(task_params)
-        @description = task_params["description"]
-        @title       = task_params["title"]
+        @database                 = SQLite3::Database.new('db/task_manager_development.db')
+        @database.results_as_hash = true
+        @description              = task_params["description"]
+        @title                    = task_params["title"]
     end
 end
